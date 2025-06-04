231c3d8c2a29a514bb8e3522074b9976cf368d8a
Brunofantoli <brunofantoli@gmail.com>
2025-06-04 09:55:17 +0200

    Add files via upload

---
+++ Temperature_sensors.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  3 14:46:13 2025
+
+@author: BrunoFantoli
+"""
+import streamlit as st
+import pandas as pd
+
+# Load the CSV
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+
+
+df = dataframe
+
+# Keep only rows with full timestamps (format: 'YYYY-MM-DD HH:MM:SS')
+df_cleaned = df[df["Time"].str.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")]
+
+
+source_id = st.text_input("Enter the source ID")
+variable_id = st.text_input("Enter the variable ID")
+file_name = st.text_input("Enter the name of the data")
+
+
+df_cleaned["source_id"] = source_id
+df_cleaned["variable_id"] = variable_id
+
+df_cleaned.columns = ["date", "value","source_id", "variable_id"]
+
+# Save the cleaned file (optional)
+output = df_cleaned.to_csv(index=False)
+
+
+
+st.download_button(
+    label="Download cleaned CSV",
+    data=output,  # Convert DataFrame to CSV string
+    file_name="OpisenseStandardDataFile_"+file_name+".csv",
+    mime="text/csv",
+)
