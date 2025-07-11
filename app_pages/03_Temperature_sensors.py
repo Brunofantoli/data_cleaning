b97fd051f189934386361c73a7bba5e23f92f715
Brunofantoli <brunofantoli@gmail.com>
2025-07-11 12:03:31 +0000

    forgot to commit last time

--- app_pages/03_Temperature_sensors.py
+++ app_pages/03_Temperature_sensors.py
@@ -18,24 +18,36 @@ variable_id = st.text_input("Enter the variable ID")
 file_name = st.text_input("Enter the name of the data")
 
 if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    uploaded_file.seek(0)  # Reset the file pointer to the beginning of the file
-    dataframe = pd.read_csv(uploaded_file, header=None, names=["Time", "Temp/°C"], skiprows=1, parse_dates=["Time"], infer_datetime_format=True) # type: ignore
-    df = dataframe
+    file_name_lower = uploaded_file.name.lower()
+    if file_name_lower.endswith('.csv'):
+        # CSV-specific logic
+        uploaded_file.seek(0)
+        dataframe = pd.read_csv(uploaded_file, header=None, names=["Time", "Temp/°C"], skiprows=1, parse_dates=["Time"], infer_datetime_format=True) # type: ignore
+        df = dataframe
+        # Can be used wherever a "file-like" object is accepted:
+        uploaded_file.seek(0)  # Reset the file pointer to the beginning of the file
+        dataframe = pd.read_csv(uploaded_file, header=None, names=["Time", "Temp/°C"], skiprows=1, parse_dates=["Time"], infer_datetime_format=True) # type: ignore
+        df = dataframe
 
-    # Force non-parsable dates to NaT (Not a Time)
-    df["Time"] = pd.to_datetime(df["Time"], errors='coerce')
+        # Force non-parsable dates to NaT (Not a Time)
+        df["Time"] = pd.to_datetime(df["Time"], errors='coerce')
 
-    # Drop rows where parsing failed
-    df_cleaned = df.dropna(subset=["Time"])
-    
-    df_cleaned["source_id"] = source_id
-    df_cleaned["variable_id"] = variable_id
-    
-    df_cleaned.columns = ["date", "value","source_id", "variable_id"]
+        # Drop rows where parsing failed
+        df_cleaned = df.dropna(subset=["Time"])
+
+    elif file_name_lower.endswith('.txt'):
+        # TXT-specific logic
+        uploaded_file.seek(0)
+        dataframe = pd.read_csv(uploaded_file, delimiter=",", encoding="latin1", index_col=0) # type: ignore
+        df_cleaned = dataframe.reset_index()[["Time", "Celsius(°C)"]]
+        
+
+    df_cleaned["source_id"] = source_id # type: ignore
+    df_cleaned["variable_id"] = variable_id # type: ignore
+    df_cleaned.columns = ["date", "value","source_id", "variable_id"] # type: ignore
     
     # Save the cleaned file (optional)
-    output = df_cleaned.to_csv(index=False)
+    output = df_cleaned.to_csv(index=False) # type: ignore
     
     st.download_button(
         label="Download",
